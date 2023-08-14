59ae3060170dae45a1f4d1cfcd41da1e424ab1b4
veranever <142037685+veranever@users.noreply.github.com>
2023-08-14 11:24:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 #create a variable to only show the full data for selected fruits
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
